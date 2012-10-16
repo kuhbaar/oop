0b316c79aba1504bc2fcd4afcf798b48c65096bf
Jakub Zarzycki <k.zacky@gmail.com>
2012-10-16 15:55:02 +0200

    1st test for member added

---
+++ Gruppe/Aufgabe1/MemberTest.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+import java.math.BigDecimal;
+import java.util.Calendar;
+
+public class MemberTest extends AbstractTest{
+	Date a,b,c,d;
+
+	@BeforeClass
+	public void InitializeCommonValues(){
+		Calendar cal = Calendar.getInstance();
+    	cal.set(2012, 9, 15);
+    	a = cal.getTime();
+
+    	cal.set(2012, 10, 4);
+    	b = cal.getTime();
+
+	    cal.set(2012, 10, 10);
+   		c = cal.getTime();
+  
+    	cal.set(2012, 10, 5);
+    	d = cal.getTime();
+	}
+	@UnitTest
+	public void testMembersAccessors() {
+    	Member m = new Member("klaus","Moor", new BigDecimal("0815"), "Chello");
+    	assertEqual(m.getName(), "klaus Moor");
+    	assertEqual(m.getPhoneNo(), new BigDecimal("0815"));
+    	assertEqual(m.getInstrument(), "Chello");
+    	assertEqual(m.getBegin(), new Date());
+    	assertEqual(m.getEnd(), new Date(Long.MAX_VALUE));
+  }
+}
