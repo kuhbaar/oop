280c20c68a687b91e6ceee5f2519210e89943228
Julian Schrittwieser <j.schrittwieser@gmail.com>
2012-10-21 11:29:27 +0200

    and one last package

--- Gruppe/Aufgabe2/Test.java
+++ Gruppe/Aufgabe2/Test.java
@@ -9,9 +9,9 @@ public class Test extends TestFramework{
     // add classes that contain tests here
     // for info how to create one, check out EventTest.java and SampleTest.java
     List<Class<? extends AbstractTest>> tests = new ArrayList<Class<? extends AbstractTest>>();
-    tests.add(EventTest.class);
-    tests.add(PlaylistTest.class);
-    tests.add(MemberTest.class);
+    tests.add(tests.EventTest.class);
+    tests.add(tests.PlaylistTest.class);
+    tests.add(tests.MemberTest.class);
 
     runTests(tests);
   }
