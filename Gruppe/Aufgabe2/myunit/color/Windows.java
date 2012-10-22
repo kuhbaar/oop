0a761a10e4bf9e22f673a06c61ed11ad1b4c0753
Julian Schrittwieser <j.schrittwieser@gmail.com>
2012-10-22 18:22:38 +0200

    rant

--- Gruppe/Aufgabe2/myunit/color/Windows.java
+++ Gruppe/Aufgabe2/myunit/color/Windows.java
@@ -1,6 +1,8 @@
 package myunit.color;
 
 public class Windows extends API {
+  // this would need to use java native api to get color, but we are not allowed
+  // to use libraries ...
   public void printSuccess(String msg) {
     System.out.println(msg);
   }
