4971ef8861403d66c0f15535a8f436824ef72e57
Julian Schrittwieser <j.schrittwieser@gmail.com>
2012-10-28 14:53:11 +0100

    two GOOD examples of dynamic binding. 3 more GOOD to go

--- Gruppe/Aufgabe3/oop/Infrastructure.java
+++ Gruppe/Aufgabe3/oop/Infrastructure.java
@@ -5,6 +5,12 @@ public class Infrastructure {
     this.description = description;
   }
   
+  /* GOOD: concrete pieces of infrastructure override this method, which allows
+     them to provide different kind of infrastructure, even when they are not a 
+     direct subclass, eg Toilets can provide Power, even though Toilet is not a
+     subclass of Power. This makes it very easy to add new infrastructure without
+     having to change old one.
+   */
   public Boolean provides(Infrastructure i) {
     return false;
   }
--- Gruppe/Aufgabe3/oop/PaymentFilter.java
+++ Gruppe/Aufgabe3/oop/PaymentFilter.java
@@ -1,5 +1,8 @@
 package oop;
 
 public interface PaymentFilter {
+  /* GOOD: concrete filters override / implement this method, which makes it
+     trivial to implement new filters without changing any other code.
+   */
   public Boolean checkPayment(Payment p);
 }
