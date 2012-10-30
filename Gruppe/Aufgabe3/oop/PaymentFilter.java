8c2eab1778712824d9515cd4f80501a19b482bc4
Julian Schrittwieser <j.schrittwieser@gmail.com>
2012-10-30 11:56:08 +0100

    some more assertions

--- Gruppe/Aufgabe3/oop/PaymentFilter.java
+++ Gruppe/Aufgabe3/oop/PaymentFilter.java
@@ -4,5 +4,8 @@ public interface PaymentFilter {
   /* GOOD: concrete filters override / implement this method, which makes it
      trivial to implement new filters without changing any other code.
    */
+  /* returns true if the payment p passes this filter (concrete functionality 
+   * provided by implementing classes)
+   */
   public Boolean checkPayment(Payment p);
 }
--- Gruppe/Aufgabe3/oop/Timespan.java
+++ Gruppe/Aufgabe3/oop/Timespan.java
@@ -3,6 +3,9 @@ package oop;
 import java.util.Date;
 
 public interface Timespan {
+  /* simple interface to get begin and end of some thing. This allows easy
+   * filtering of objects by date ranges 
+   */
   public Date getBegin();
   public Date getEnd();
 }
