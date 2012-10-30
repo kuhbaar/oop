0af2bd4fa55df21e2f84f94856d91c8bc82b8057
Jakub Zarzycki <k.zacky@gmail.com>
2012-10-30 12:01:55 +0100

    asserts in Musicgroup + 1 BAD

--- Gruppe/Aufgabe3/oop/MusicGroup.java
+++ Gruppe/Aufgabe3/oop/MusicGroup.java
@@ -31,7 +31,7 @@ public class MusicGroup {
     this.current_playlist = new ArrayList<Song>();
     this.various_payments = new ArrayList<Payment>();
   }
-
+  /* adds the Member m to the current_members with current date as Join, also adds it to the members*/
   public void addMember(Member m){
     Member mk= m.join();
     current_members.add(mk);
@@ -40,7 +40,6 @@ public class MusicGroup {
 
   //deletes the Member from current_members and adds a new Member copying the old
   //returns the Member
-
   public Member subToMember(Member m) {
     deleteMember(m);
     m=new Member(m);
@@ -50,7 +49,6 @@ public class MusicGroup {
 
   //deletes a Member from current_members and adds a new substitute copying the old Member
   //returns the Substitute
-
   public Substitute memberToSub(Member m) {
     deleteMember(m);
     Substitute s=new Substitute(m);
@@ -58,16 +56,17 @@ public class MusicGroup {
     return s;
   }
   
+  /* deletes the Member m from current_members, also adds the member with current date as Leave to the members */
   public void deleteMember(Member m) {
     current_members.remove(m);
     members.remove(m);
     members.add(m.leave());
   }
-
+  /* returns the copy of current_members */
   public List<Member> getCurrentMembers() {
     return new ArrayList<Member>(current_members);
   }
-
+  /* returns a list of members inbetween begin and end */
   public List<Member> getMembers(Date begin, Date end){
     return elementsBetween(members, begin, end, Member.class);
   }
@@ -85,8 +84,6 @@ public class MusicGroup {
   }
 
   //removes a Song from the current_playlist and the playlist and adds a copy of it, while setting the copies gone to the current date
-
-  
   public void deleteSong(Song s) {
     current_playlist.remove(s);
     playlist.remove(s);
@@ -112,7 +109,6 @@ public class MusicGroup {
   }
 
   //returns a copy of the playlist, using only Songs active during at least a part of the Timespan between begin and end
-
 
   public List<Song> getPlaylist(Date begin,Date end) {
     return elementsBetween(playlist,begin,end,Song.class);
@@ -153,7 +149,6 @@ public class MusicGroup {
 
   //returns the available Members for gigs (all members except members of the class substitute, which have not been attending 3 rehearsal within the last 7 days)
   
-
   public ArrayList<Member> availableMembers(List<Member> members){
 
     ArrayList<Member> temp= new ArrayList<Member>();
@@ -164,30 +159,35 @@ public class MusicGroup {
     }
     return temp;
   }
-
+  /* adds the Event e to the Events, Also informs the current_members by sending a message to them*/
   public void newEvent(Event e){
     events.add(e);
     sendMessageToMembers(e);
   }
-
+  /* deletes the Event e from events and adds it to the del_events, Also informs the current_members by sending a message to them*/
   public void deleteEvent(Event e){
     del_events.add(e);
     events.remove(e);
     sendMessageToMembers(e);
   }
 
+  /* swaps the Event e in the events with the changed Event change.
+    The unchanged Event e is being saved in Event change for undoChangeEvent(..),
+    Also informs the current_members by sending a message to them*/
   public void changeEvent(Event e, Event change){
+    /*BAD: object coupling too strong. It would be better to create a new generic class for Stacks and use it for saving and undoing changes*/
     change.save(e);
     events.add(events.indexOf(e), change);
     sendMessageToMembers(change);
   }
-  
+  /* Recalls the previous version of Event change and replaces change with it.
+    Also informs the current_members by sending a message to them*/
   public void undoChangeEvent(Event change){
     Event undochange= new Event(change.undo());
     events.set(events.indexOf(change),undochange);
     sendMessageToMembers(change);
   }
-
+  /* adds the Event e to message list of Member to for all Member in current_members*/
   public void sendMessageToMembers(Event e){
     for(Member m: current_members){
       m.sendMessage(e);
