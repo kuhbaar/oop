1d8cb155d0afcedeee91bae48ed1b50c0dffe005
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2012-10-30 10:37:19 +0100

    show files only edited by one person

--- count_lines.py
+++ count_lines.py
@@ -8,10 +8,13 @@ root = "./Gruppe/Aufgabe3/"
 authors = {}
 files_touched_by = {}
 total_lines = 0
+authors_per_file = {}
+files_owned_by = {}
 
 for mail in ['j.schrittwieser@gmail.com', 'k.zacky@gmail.com', 'brummbaerig@gmail.com']:
   authors[mail] = 0
   files_touched_by[mail] = []
+  files_owned_by[mail] = []
 
 def count_authors(filename):
   involved_authors = {}
@@ -24,8 +27,11 @@ def count_authors(filename):
     except:
       print line
 
+  authors_per_file[filename] = []
   for author in involved_authors:
     files_touched_by[author].append(filename)
+    authors_per_file[filename].append(author)
+  
 
 def count_lines(filename):
   global total_lines
@@ -41,9 +47,16 @@ def forall_files(dirname, fun):
       filename = "%s/%s" % (root, f)
       fun(filename)
 
+def clean_filename(name):
+  return (name.split("/")[-1]).split(".")[0]
 
 forall_files(root, count_authors)
 forall_files(root, count_lines)
+
+
+for file, auths in authors_per_file.iteritems():
+  if len(auths) == 1:
+    files_owned_by[auths[0]].append(clean_filename(file))
 
 
 for author, lines in sorted(authors.iteritems(), key=operator.itemgetter(1), reverse=True):
@@ -51,5 +64,13 @@ for author, lines in sorted(authors.iteritems(), key=operator.itemgetter(1), rev
 
 print
 for author, files in files_touched_by.iteritems():
-  filenames = [(f.split("/")[-1]).split(".")[0] for f in files]
+  filenames = [clean_filename(f) for f in files]
   print "%30s edited %s" % (author, ', '.join(sorted(filenames)))
+
+print
+for author, files in files_owned_by.iteritems():
+  filenames = [clean_filename(f) for f in files]
+  print "%30s owns %s" % (author, ', '.join(sorted(filenames)))
+
+	
+
