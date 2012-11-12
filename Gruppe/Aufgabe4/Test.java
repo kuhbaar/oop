c850e9f4f53d62bcc49308c18e28db122135a3d1
Julian Schrittwieser <j.schrittwieser@gmail.com>
2012-11-12 22:58:17 +0100

    and some testing

--- Gruppe/Aufgabe4/Test.java
+++ Gruppe/Aufgabe4/Test.java
@@ -1,5 +1,19 @@
 public class Test {
   public static void main(String[] args) {
+    Pict p = new Box(10, 4, '*', '.');
+
+    System.out.println(p);
+
+    p.scale(2.4);
+
+    System.out.println(p);
+
+    Pict q = new ClearBox(5, 3);
+    System.out.println(q);
+
+
+    q.scale(3.4);
+    System.out.println(q);
 
   }
 }
