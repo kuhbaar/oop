7973d47d213f84538d6dfc21ac619be668a23d7f
Julian Schrittwieser <j.schrittwieser@gmail.com>
2012-11-12 22:57:56 +0100

    implemented Box

--- Gruppe/Aufgabe4/Box.java
+++ Gruppe/Aufgabe4/Box.java
@@ -1,22 +1,57 @@
 public class Box implements Pict {
-  private final int width;
-  private final int height;
+  protected final int width;
+  protected final int height;
   private final char border;
   private final char body;
 
+  private double scale;
 
   public Box(int width, int height, char border, char body) {
-    this->width = width;
-    this->height = height;
-    this->border = border;
-    this->body = body;
+    this.width = width;
+    this.height = height;
+    this.border = border;
+    this.body = body;
+    this.scale = 1.0;
   }
   
   public String toString() {
-    throw new RuntimeException("not implemented");
+    int scaledHeight = (int) Math.ceil(height * scale);
+    int scaledWidth = (int) Math.ceil(width * scale);
+
+    if(scaledHeight == 0 && scaledWidth == 0) return "";
+    if(scaledHeight == 1) return borderLine(scaledWidth);
+    if(scaledHeight == 2) 
+      return borderLine(scaledWidth) + "\n" + borderLine(scaledWidth);
+
+    String out = borderLine(scaledWidth);
+    for(int i = 0; i < scaledHeight - 2; i++)
+      out += "\n" + bodyLine(scaledWidth);
+
+    return out + "\n" + borderLine(scaledWidth);
   }
 
   public void scale(double factor) {
-    throw new RuntimeException("not implemented");
+    assert(factor >= 0.1);
+    assert(factor <= 10.0);
+
+    this.scale = factor;
+  }
+
+  private String borderLine(int w) {
+    String out = "";
+    for(int i = 0; i < w; i++) 
+      out += border;
+    return out;
+  }
+
+  private String bodyLine(int w) {
+    if(1 == w) return "" + border;
+    else if(2 == w) return "" + border + border;
+
+    String out = "" + border;
+    for(int i = 0; i < w - 2; i++) 
+      out += body;
+
+    return out + border;
   }
 }
