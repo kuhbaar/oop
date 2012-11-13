81e0f894a0bbecb82a47fd9af519aa40324d5be3
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2012-11-13 21:51:28 +0100

    FreeBox zusicherungen

--- Gruppe/Aufgabe4/FreeBox.java
+++ Gruppe/Aufgabe4/FreeBox.java
@@ -3,15 +3,19 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class FreeBox implements Pict {
-  protected final List<String> text;
+  private final List<String> text;
 
   private double scale;
 
+  /* erstellt eine Box mit konstantem Text als Inhalt */
   public FreeBox(List<String> text) {
     this.text = new ArrayList<String>(text);
     this.scale = 1.0;
   }
-  
+
+  /* verwandelt die Box skaliert mit dem gesetzten Faktor in eine Zeichenkette.
+     Ist der Faktor < 1, werden Teile des Texts weggelassen (rechte und untere 
+     Rand), ist er größer als 1 wird der Text entsprechend wiederholt */
   public String toString() {
     final int height = text.size();
     final int width = text.size() > 0 ? text.get(0).length() : 0;
@@ -27,6 +31,7 @@ public class FreeBox implements Pict {
     return out.substring(0, out.length() - 1);  /* strip final newline */
   }
 
+  /* setzt die Skalierung zwischen 0.1 und 10.0 */
   public void scale(double factor) {
     assert(factor >= 0.1);
     assert(factor <= 10.0);
