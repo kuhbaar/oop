c149e4f9c9c04bdea6f9d43333ff6e650981fcda
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2012-11-13 21:49:28 +0100

    DarkBox zusicherungen

--- Gruppe/Aufgabe4/DarkBox.java
+++ Gruppe/Aufgabe4/DarkBox.java
@@ -5,6 +5,8 @@ public class DarkBox implements Pict {
   private double scale;
   private Box b;
 
+  /* erstellt eine Box mit konstanter Höhe und Breite, das Zeichen für den Inhalt
+     (Rand gibt es keinen) ist aber veränderlich! */
   public DarkBox(int width, int height, char c) {
     this.b = new Box(width, height, c, c);
     this.width = width;
@@ -12,10 +14,12 @@ public class DarkBox implements Pict {
     this.scale = 1.0;
   }
   
+  /* verwandelt die Box skaliert mit dem gesetzten Faktor in eine Zeichenkette */
   public String toString() {
     return b.toString();
   }
 
+  /* setzt die Skalierung zwischen 0.1 und 10.0 */
   public void scale(double factor) {
     assert(factor >= 0.1);
     assert(factor <= 10.0);
@@ -24,6 +28,7 @@ public class DarkBox implements Pict {
     b.scale(factor);
   }
 
+  /* setzt das Zeichen für den Inhalt */
   public void setChar(char c) {
     this.b = new Box(this.width, this.height, c, c);
     this.b.scale(this.scale);
