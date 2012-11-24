972aeef5dfe4af4dd12d466e2973eb0f12c20dbc
Jakub Zarzycki <k.zacky@gmail.com>
2012-11-24 21:42:36 +0100

    The beginning Part 2

--- Gruppe/Aufgabe6/Android.java
+++ Gruppe/Aufgabe6/Android.java
@@ -1,3 +1,11 @@
 public abstract class Android{
-  private String seriennr;
+  private final String seriennr;
+  private Skin s;
+  private Software sw;
+
+  public Android(String n, Skin s, Software sw){
+    this.seriennr = n;
+    this.s = s;
+    this.sw = sw;
+  }
 }
