de587b91540be4a82eee11f202c74b40be2cb58b
Julian Schrittwieser <j.schrittwieser@gmail.com>
2012-11-25 23:24:31 +0100

    test AndroidList and inserting

--- Gruppe/Aufgabe6/AndroidList.java
+++ Gruppe/Aufgabe6/AndroidList.java
@@ -28,12 +28,19 @@ public class AndroidList {
   public Iterator<Android> iterator() {
     return this.droids.iterator();
   }
-
+  /* Die Methode insert fügt einen Androiden mit eindeutiger Seriennummer und
+    allen Ausstattungsdetails in die Liste ein und prüft die Bedingungen der
+    Androide-Verordnung. Sind die Bedingungen nicht erfüllt, bleibt die Liste
+    unverändert, und ein entsprechender Fehlercode wird zurückgegeben. Kommt
+    ein Androide mit derselben Seriennummer bereits in der Liste vor, so handelt
+    es sich um eine Änderung, sonst um die Auslieferung eines neuen Androiden.
+    Als Ausstattungsdetails bekommt jeder Android ein Sensoren-Aktoren-Kit,
+    eine Skin und eine Software mit. */
   /* true wenn insert erfolgreich war, sonst false */
   public boolean insert(Android a) {
     List<Android> aList = a.accept(new Inspector(this.droids));
     aList.add(a);
     return this.droids == aList;
-/* Die Methode insert fügt einen Androiden mit eindeutiger Seriennummer und allen Ausstattungsdetails in die Liste ein und prüft die Bedingungen der Androide-Verordnung. Sind die Bedingungen nicht erfüllt, bleibt die Liste unverändert, und ein entsprechender Fehlercode wird zurückgegeben. Kommt ein Androide mit derselben Seriennummer bereits in der Liste vor, so handelt es sich um eine Änderung, sonst um die Auslieferung eines neuen Androiden. Als Ausstattungsdetails bekommt jeder Android ein Sensoren-Aktoren-Kit, eine Skin und eine Software mit. */
+
   }
 }
--- Gruppe/Aufgabe6/Test.java
+++ Gruppe/Aufgabe6/Test.java
@@ -5,9 +5,8 @@ public class Test {
     Android a = new Kaempfer("007", new HochfesterSkin(),
       new KaempferSoftware("007", new Sicherheitsstufe5()));
 
-    ArrayList<Android> list = new ArrayList<Android>();
-    Inspector i = new Inspector(list);
-    a.accept(i);
+    AndroidList list = new AndroidList();
+    System.out.println(list.insert(a));
   }
 
 }
