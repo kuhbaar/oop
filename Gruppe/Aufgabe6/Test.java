c37a5563e7ce348d6f88b896183fe64714d42ff9
Julian Schrittwieser <j.schrittwieser@gmail.com>
2012-11-25 23:42:42 +0100

    simple testing

--- Gruppe/Aufgabe6/Test.java
+++ Gruppe/Aufgabe6/Test.java
@@ -5,8 +5,19 @@ public class Test {
     Android a = new Kaempfer("007", new HochfesterSkin(),
       new KaempferSoftware("007", new Sicherheitsstufe5()));
 
+    Android b = new Hilfskraft("001", new GepanzerterSkin(),
+      new HilfskraftSoftware("001", new Sicherheitsstufe1()));
+
     AndroidList list = new AndroidList();
-    System.out.println(list.insert(a));
+    expect(list.insert(a), true, "gueltiger kaempfer");
+    expect(list.insert(b), false, "falscher skin");
+  }
+
+  public static void expect(boolean a, boolean b, String msg) {
+    if(a != b)
+      System.out.println("!!! FAIL: " + msg + " !!!");
+    else
+      System.out.println("  success");
   }
 
 }
