4aa641993e6a7cd593901e3dc749c5a3136e821b
Julian Schrittwieser <j.schrittwieser@gmail.com>
2012-11-26 12:26:40 +0100

    change inspector to interface

--- Gruppe/Aufgabe6/Inspector.java
+++ Gruppe/Aufgabe6/Inspector.java
@@ -1,51 +1,14 @@
 import java.util.List;
+import java.util.ArrayList;
 
-public class Inspector {
-  private List<Android> droids;
-
-  public Inspector(List<Android> droids) {
-    this.droids = droids;
-  }
-
-  public List<Android> visit(Android a) {
-    System.out.println("Android");
-    return droids;
-  }
-
-  public List<Android> visit(Hilfskraft a) {
-    throw new RuntimeException("not implemented");
-  }
-
-  public List<Android> visit(Gesellschafter a) {
-    throw new RuntimeException("not implemented");
-  }
-
-  public List<Android> visit(Bauarbeiter a) {
-    throw new RuntimeException("not implemented");
-  }
-
-  public List<Android> visit(ServiceTechniker a) {
-    throw new RuntimeException("not implemented");
-  }
-
-  public List<Android> visit(Transportarbeiter a) {
-    throw new RuntimeException("not implemented");
-  }
-
-  public List<Android> visit(Objektbewacher a) {
-    throw new RuntimeException("not implemented");
-  }
-
-  public List<Android> visit(Leibwaechter a) {
-    throw new RuntimeException("not implemented");
-  }
-
-  public List<Android> visit(Kaempfer a) {
-    System.out.println("Kaempfer");
-    this.droids = a.inspectSkin(new BeschuetzerSkinInspector(this.droids));
-    this.droids = a.inspectSoftware(new KaempferSoftwareInspector(this.droids));
-    this.droids = a.inspectSecurity(new Stufe5SoftwareSicherheitsInspector(this.droids));
-
-    return this.droids;
-  }
+public abstract class Inspector {
+  public List<Android> visit(Android a) { return new ArrayList<Android>(); }
+  public List<Android> visit(Hilfskraft a) { return new ArrayList<Android>(); }
+  public List<Android> visit(Gesellschafter a) { return new ArrayList<Android>(); }
+  public List<Android> visit(Bauarbeiter a) { return new ArrayList<Android>(); }
+  public List<Android> visit(ServiceTechniker a) { return new ArrayList<Android>(); }
+  public List<Android> visit(Transportarbeiter a) { return new ArrayList<Android>(); }
+  public List<Android> visit(Objektbewacher a) { return new ArrayList<Android>(); }
+  public List<Android> visit(Leibwaechter a) { return new ArrayList<Android>(); }
+  public List<Android> visit(Kaempfer a) { return new ArrayList<Android>(); }
 }
---
+++ Gruppe/Aufgabe6/RuleInspector.java
@@ -0,0 +1,51 @@
+import java.util.List;
+
+public class RuleInspector extends Inspector {
+  private List<Android> droids;
+
+  public RuleInspector(List<Android> droids) {
+    this.droids = droids;
+  }
+
+  public List<Android> visit(Android a) {
+    System.out.println("Android");
+    return droids;
+  }
+
+  public List<Android> visit(Hilfskraft a) {
+    throw new RuntimeException("not implemented");
+  }
+
+  public List<Android> visit(Gesellschafter a) {
+    throw new RuntimeException("not implemented");
+  }
+
+  public List<Android> visit(Bauarbeiter a) {
+    throw new RuntimeException("not implemented");
+  }
+
+  public List<Android> visit(ServiceTechniker a) {
+    throw new RuntimeException("not implemented");
+  }
+
+  public List<Android> visit(Transportarbeiter a) {
+    throw new RuntimeException("not implemented");
+  }
+
+  public List<Android> visit(Objektbewacher a) {
+    throw new RuntimeException("not implemented");
+  }
+
+  public List<Android> visit(Leibwaechter a) {
+    throw new RuntimeException("not implemented");
+  }
+
+  public List<Android> visit(Kaempfer a) {
+    System.out.println("Kaempfer");
+    this.droids = a.inspectSkin(new BeschuetzerSkinInspector(this.droids));
+    this.droids = a.inspectSoftware(new KaempferSoftwareInspector(this.droids));
+    this.droids = a.inspectSecurity(new Stufe5SoftwareSicherheitsInspector(this.droids));
+
+    return this.droids;
+  }
+}
