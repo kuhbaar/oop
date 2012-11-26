4c2fa4d7568eeeb211e0ca4336d965d2bf36ced2
Julian Schrittwieser <j.schrittwieser@gmail.com>
2012-11-26 10:17:11 +0100

    added stubs for android visit methods

--- Gruppe/Aufgabe6/Inspector.java
+++ Gruppe/Aufgabe6/Inspector.java
@@ -12,6 +12,34 @@ public class Inspector {
     return droids;
   }
 
+  public List<Android> visit(Hilfskraft a) {
+    throw new RuntimeException("not implemented");
+  }
+
+  public List<Android> visit(Gesellschafter a) {
+    throw new RuntimeException("not implemented");
+  }
+
+  public List<Android> visit(Bauarbeiter a) {
+    throw new RuntimeException("not implemented");
+  }
+
+  public List<Android> visit(ServiceTechniker a) {
+    throw new RuntimeException("not implemented");
+  }
+
+  public List<Android> visit(Transportarbeiter a) {
+    throw new RuntimeException("not implemented");
+  }
+
+  public List<Android> visit(Objektbewacher a) {
+    throw new RuntimeException("not implemented");
+  }
+
+  public List<Android> visit(Leibwaechter a) {
+    throw new RuntimeException("not implemented");
+  }
+
   public List<Android> visit(Kaempfer a) {
     System.out.println("Kaempfer");
     this.droids = a.inspectSkin(new BeschuetzerSkinInspector(droids));
