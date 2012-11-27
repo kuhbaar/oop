2d574da3cc506eea9d33a907bc4c4f7bf1aef79e
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2012-11-27 22:32:19 +0100

    leistungs inspector

---
+++ Gruppe/Aufgabe6/LeistungsInspector.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class LeistungsInspector extends SicherheitsstufenInspector {
+  private final List<Android> droids;
+  private final int leistung;
+
+  public LeistungsInspector(List<Android> droids, double leistung) {
+    this.droids = droids;
+    this.leistung = Math.ceil(leistung);
+  }
+
+  public List<Android> visit(Sicherheitsstufe1 s) { return droids; }
+  public List<Android> visit(Sicherheitsstufe2 s) { return droids; }
+  public List<Android> visit(Sicherheitsstufe3 s) { 
+    HashMap<Integer, List<Android>> m = new HashMap<Integer, List<Android>>();
+    for(int i = 0; i < 5; i++)
+      m.put(i, droids);
+
+    List<Android> as = m.get(this.leistung);
+
+
+    return as == null ? new ArrayList<Android>() : as;
+  }
+  public List<Android> visit(Sicherheitsstufe4 s) { return droids; }
+  public List<Android> visit(Sicherheitsstufe5 s) { return droids; }
+}
