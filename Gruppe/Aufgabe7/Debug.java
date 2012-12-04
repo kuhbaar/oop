bdac1e077160909a4a76949ab1bc07b4add68e14
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2012-12-04 11:26:36 +0100

    More comments

--- Gruppe/Aufgabe7/Debug.java
+++ Gruppe/Aufgabe7/Debug.java
@@ -1,17 +1,23 @@
 public class Debug {
   private final static boolean debug = false;
 
+  /* retrieve the callers location from the stack */
   private static String getLocation() {
     StackTraceElement l = new Throwable().getStackTrace()[2];
-    return l.getFileName()+", "+l.getClassName()+"/"+l.getMethodName()+":"+l.getLineNumber();
+    return l.getFileName()+", " +l.getMethodName()+"."+l.getLineNumber();
   }
 
+  /* logs an info message to stdout with the following format:
+     [info] <threadname> @ <filename>, <methodname>.<lineno>: <msg> */
   public static void info(String msg) {
-    if(debug) System.out.println(String.format("%s @ %s: %s",
+    if(debug) System.out.println(String.format("[info] %s @ %s: %s",
       Thread.currentThread().getName(), getLocation(), msg));
   }
 
+  /* logs an info message to stderr with the following format:
+     [error] <threadname> @ <filename>, <methodname>.<lineno>: <msg> */
   public static void error(String msg) {
-    if(debug) System.err.println(msg);
+    if(debug) System.err.println(String.format("[error] %s @ %s: %s",
+      Thread.currentThread().getName(), getLocation(), msg));
   }
 }
