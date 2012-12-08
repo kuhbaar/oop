4ff73af15257fd918cb982ccd69fdcee661a4b38
Jakub Zarzycki <k.zacky@gmail.com>
2012-12-08 15:06:00 +0100

    Runtime Annotation

--- Gruppe/Aufgabe8/Author.java
+++ Gruppe/Aufgabe8/Author.java
@@ -1,3 +1,6 @@
+//Runtime, damit man die Authoren mittels Reflektion bestimmen können (siehe letzter Punkt beim Testen)
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
 public @interface Author{
   String author() default "none";
 }
